b7fb0dbcb79fb7de0a59d7a675e610ac7feb99de
Nick Laycock <nlaycock@hourlynerd.com>
2014-10-10 13:17:41 +1100

    new files

---
+++ app/assets/javascripts/jts.js
@@ -0,0 +1,12 @@
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require jquery-1.11.1.min
+//= require bootstrap.min
+//= require modernizr.custom
+//= require classie
+//= require elastic_grid.min
+//= require owl.carousel.min
+//= require lightbox.min
+//= require themes
---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,32 @@
+class AccessController < ApplicationController
+  def index
+    # display text and links. Nothing to return
+  end
+
+  def login
+    # login form
+  end
+
+  def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = User.where(:username => params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+    if authorized_user
+      # mark user as logged in
+      session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
+      flash[:notice] = "You are now logged in."
+      redirect_to(:action => 'index')
+    else
+      flash[:notice] = "Invalid username/password combination."
+      redirect_to(:action => 'login')
+    end
+  end
+
+  def logout
+
+  end
+end
